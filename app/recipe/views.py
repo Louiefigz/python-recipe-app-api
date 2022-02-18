a0d0cf3ce631e748a8f81b734f7fe9ded7fb1556
Aaron Figueroa <aaronfigueroa@GOTfan.local>
2022-02-18 14:13:31 -0600

    Add Ingredient Feature and unit test

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,8 +1,9 @@
+from math import perm
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 from recipe import serializers
 
@@ -23,3 +24,16 @@ class TagViewSet(viewsets.GenericViewSet,
         # Create a new tag
         # This is using the CreateModelMixin mixin
         serializer.save(user=self.request.user)
+
+class IngredientViewSet(viewsets.GenericViewSet, 
+                        mixins.ListModelMixin):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+    
+
+
